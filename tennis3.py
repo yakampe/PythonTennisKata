f8bd0a3666e979f7a7d7213bed4bb394fcc8d736
yakampe <yanis.kampe.cv@gmail.com>
2023-11-23 11:31:44 +0000

    tennis3 refactored

--- tennis3.py
+++ tennis3.py
@@ -2,24 +2,48 @@
 
 class TennisGame3:
     def __init__(self, player1Name, player2Name):
-        self.p1N = player1Name
-        self.p2N = player2Name
-        self.p1 = 0
-        self.p2 = 0
+        self.__player_one_name = player1Name
+        self.__player_two_name = player2Name
+        self.__player_one_score = 0
+        self.__player_two_score = 0
 
-    def won_point(self, n):
-        if n == "player1":
-            self.p1 += 1
+    def won_point(self, scoring_player):
+        if scoring_player == self.__player_one_name:
+            self.__player_one_score += 1
         else:
-            self.p2 += 1
+            self.__player_two_score += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+        is_endgame = not ((self.__player_one_score < 4 and self.__player_two_score < 4) and (
+                self.__player_one_score + self.__player_two_score < 6))
+        if is_endgame:
+            return self.__return_endgame_score()
         else:
-            if (self.p1 == self.p2):
-                return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
-            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+            return self.__return_ongoing_score()
+
+    def __return_ongoing_score(self):
+        score_names = ["Love", "Fifteen", "Thirty", "Forty"]
+        score = score_names[self.__player_one_score]
+
+        is_tie = self.__player_one_score == self.__player_two_score
+
+        if is_tie:
+            return score + "-All"
+        else:
+            return score + "-" + score_names[self.__player_two_score]
+
+    def __return_endgame_score(self):
+        is_tie = self.__player_one_score == self.__player_two_score
+        if is_tie:
+            return "Deuce"
+
+        is_advantage_stage = ((self.__player_one_score - self.__player_two_score) * (
+                self.__player_one_score - self.__player_two_score) == 1)
+
+        if is_advantage_stage:
+            return "Advantage " + self.__get_winning_player()
+        else:
+            return "Win for " + self.__get_winning_player()
+
+    def __get_winning_player(self):
+        return self.__player_one_name if self.__player_one_score > self.__player_two_score else self.__player_two_name
