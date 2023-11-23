643b4815af5179522d17f06a18addae2669a1197
yakampe <yanis.kampe.cv@gmail.com>
2023-11-23 15:43:38 +0000

    yanis example

--- tennis3.py
+++ tennis3.py
@@ -2,24 +2,45 @@
 
 class TennisGame3:
     def __init__(self, player1Name, player2Name):
-        self.p1N = player1Name
-        self.p2N = player2Name
-        self.p1 = 0
-        self.p2 = 0
+        self.__player_one_name = player1Name
+        self.__player_two_name = player2Name
+        self.__player_one_score = 0
+        self.__player_two_score = 0
 
-    def won_point(self, n):
-        if n == "player1":
-            self.p1 += 1
+    def won_point(self, players_who_won):
+        if players_who_won == self.__player_one_name:
+            self.__player_one_score += 1
         else:
-            self.p2 += 1
+            self.__player_two_score += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+        is_early_game = (self.__player_one_score < 4 and self.__player_two_score < 4) and (
+                self.__player_one_score + self.__player_two_score < 6)
+
+        if is_early_game:
+            return self.__get_early_game_score()
         else:
-            if (self.p1 == self.p2):
-                return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
-            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+            return self.__get_end_game_score()
+
+    def __get_end_game_score(self):
+        same_score = self.__player_one_score == self.__player_two_score
+        if same_score:
+            return "Deuce"
+        winning_player = self.__get_winning_player()
+        if (self.__player_one_score - self.__player_two_score) * (
+                self.__player_one_score - self.__player_two_score) == 1:
+            return "Advantage " + winning_player
+        else:
+            return "Win for " + winning_player
+
+    def __get_early_game_score(self):
+        score_names = ["Love", "Fifteen", "Thirty", "Forty"]
+        same_score = self.__player_one_score == self.__player_two_score
+        score = score_names[self.__player_one_score]
+        if same_score:
+            return score + "-All"
+        else:
+            return score + "-" + score_names[self.__player_two_score]
+
+    def __get_winning_player(self):
+        return self.__player_one_name if self.__player_one_score > self.__player_two_score else self.__player_two_name
