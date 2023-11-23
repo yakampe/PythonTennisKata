3bc939cd881e14d462e9c0d5e719a4e34e05be0f
yakampe <yanis.kampe.cv@gmail.com>
2023-11-23 12:39:41 +0000

    removing unused methods

--- score_counter.py
+++ score_counter.py
@@ -9,10 +9,6 @@ class ScoreCounter:
     def player_two_scored(self):
         self.__player_two_score += 1
 
-    def is_endgame(self):
-        return not ((self.__player_one_score < 4 and self.__player_two_score < 4) and (
-                self.__player_one_score + self.__player_two_score < 6))
-
     def player_one_score(self):
         return self.__player_one_score
 
